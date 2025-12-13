6b7f6acc63cf1a95db8f5c74ab4886872b485f89
Shamaniks <yshamanskiy@gmail.com>
2025-12-13 20:26:37 +0300

    Поправил main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from config.settings import TOKEN
 async def main():
     application = build(TOKEN)
     setup_background_tasks(application)
-    await application.run_polling()
+    application.run_polling()
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    main()
