f6f79cab675e801e02441be4273fe3d33cbe9b1f
Shamaniks <yshamanskiy@gmail.com>
2025-12-13 20:25:27 +0300

    Поправил get в _queue (он не должен просить аргументы

--- bus/_queue.py
+++ bus/_queue.py
@@ -5,7 +5,7 @@ _event_queue = asyncio.Queue()
 async def put(event):
     await _event_queue.put(event)
 
-async def get(event):
+async def get():
     await _event_queue.get(event)
 
 def task_done():
