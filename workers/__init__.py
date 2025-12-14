2db50bbf80348b021dd54c5671e84ce4d6c905db
Shamaniks <yshamanskiy@gmail.com>
2025-12-14 19:55:58 +0300

    Этого тут не должно быть

--- workers/__init__.py
+++ workers/__init__.py
@@ -2,4 +2,4 @@ from .dispatcher import register
 from domain.events import UserMessageEvent
 from handlers.answer_handler import handle_user_message_model
 
-# register(UserMessageEvent, handle_user_message_model)
+register(UserMessageEvent, handle_user_message_model)
