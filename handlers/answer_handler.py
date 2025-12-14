7ca0190d0f23c95315ef16f1e27a34916087e4d4
RuYaV <ruban-owk@yandex.ru>
2025-12-14 18:29:26 +0300

    Добавил комментарии

--- handlers/answer_handler.py
+++ handlers/answer_handler.py
@@ -4,13 +4,17 @@ from similarity import load_answers_and_embeddings, ask, embedding_model
 answers, answer_embeddings = load_answers_and_embeddings()
 
 async def handle_user_message_model(event, application):
+    """Получаем лучший ответ и его вероятность из нейросети"""
     best_answer, similarity = ask(
         question=event.text,
         answers=answers,
         answer_embeddings=answer_embeddings
     )
 
+    """Обрабатываем вариант, когда нейросеть не уверена в своём ответе"""
     reply = best_answer if best_answer else "Ответ не найден"
+
+    """Отправляем в чат полученный ответ"""
     await application.bot.send_message(
         chat_id=event.chat_id,
         text=reply
